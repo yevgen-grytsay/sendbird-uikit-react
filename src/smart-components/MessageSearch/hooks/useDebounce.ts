09e169e43b862fc7ff058c0548984347d5b20b01
Sravan S <sravan@sendbird.com>
2021-11-05 16:34:19 +0900

    Add comments to clarify useDebounce

--- src/smart-components/MessageSearch/hooks/useDebounce.ts
+++ src/smart-components/MessageSearch/hooks/useDebounce.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 
-
+// thanks https://usehooks-ts.com/react-hook/use-debounce
+// should move to generic hooks if we have to reuse it later
 function useDebounce<T>(value: T, delay?: number): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
   useEffect(() => {
